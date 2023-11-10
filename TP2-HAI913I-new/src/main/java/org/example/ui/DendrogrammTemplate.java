a1d6d28b3c3e564214105f22d62d645a9d9a2232
mathieu <mathieucazeres@gmail.com>
2023-11-10 21:21:46 +0100

    dendrogramm je rage

--- TP2-HAI913I-new/src/main/java/org/example/ui/DendrogrammTemplate.java
+++ TP2-HAI913I-new/src/main/java/org/example/ui/DendrogrammTemplate.java
@@ -2,6 +2,8 @@ package org.example.ui;
 
 
 import com.mxgraph.layout.hierarchical.mxHierarchicalLayout;
+import com.mxgraph.model.mxCell;
+import com.mxgraph.model.mxGeometry;
 import com.mxgraph.swing.mxGraphComponent;
 import com.mxgraph.util.mxRectangle;
 import com.mxgraph.view.mxGraph;
@@ -13,7 +15,11 @@ import org.example.service.ClusterService;
 import org.example.service.DendrogrammService;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.*;
+import java.util.List;
 
 @NoArgsConstructor
 public class DendrogrammTemplate {
@@ -23,15 +29,21 @@ public class DendrogrammTemplate {
     private static final int MARGIN = 100;
     private JFrame frame = new JFrame("");
 
-    private static final float CP = 0.16F;
+    private static final float CP = 0.15F;
     private int nModule ;
     private Map<Set<String>,Object> sommets = new HashMap<>();
-    private Object parent;
+    private Object feuille;
+
+
 
     public void createGraph(ArrayList<Coupling> couplings, int m) {
         SwingUtilities.invokeLater(() -> {
             mxGraph graph = initGraph(frame);
             DendrogrammService service = new DendrogrammService();
+            // Créer un bouton de dézoom
+
+
+
 
             nModule = Math.abs(m/2);
 //            System.err.println(nModule);
@@ -43,6 +55,7 @@ public class DendrogrammTemplate {
             HashMap<Set<String>, List<Set<String>>> keys = service.initCoupleClasses(couplings);
             LinkedHashMap<Set<String>,Float> values = service.initTest(couplings);
 //            PairCluster pairCluster = PairCluster.builder().classes(new LinkedList<LinkedList<Object>>()).values(new LinkedList<Float>()).build();
+
 
             while (!values.isEmpty())
             {
@@ -63,8 +76,6 @@ public class DendrogrammTemplate {
                 // get sommet si ils existent
                 Object sommetA = sommets.get(classeA);
                 Object sommetB = sommets.get(classeB);
-
-
                 if (sommetA == null)
                 {
                     if (sommetB == null)
@@ -80,21 +91,19 @@ public class DendrogrammTemplate {
                 //Sinon on les crees
                 if (sommetA == null) {
 
-                    sommetA = graph.insertVertex(parent, null, classeA, 20, 20, 80, 30);
+                    sommetA = graph.insertVertex(feuille, null, classeA, 20, 20, 80, 30);
                     sommets.put(classeA, sommetA);
                 }
 
                 if (sommetB == null) {
-
-
-                    sommetB = graph.insertVertex(parent, null, classeB, 20, 20, 80, 30);
+                    sommetB = graph.insertVertex(feuille, null, classeB, 20, 20, 80, 30);
                     sommets.put(classeB, sommetB);
+
                 }
 
                 // On cree le noued de couplage
-                Object sommetAB = graph.insertVertex(parent, null, valueAB, 20, 20, 80, 30);
+                Object sommetAB = graph.insertVertex(feuille, null, valueAB, 20, 20, 80, 30);
                 sommets.put(classeAB,sommetAB);
-
 
                 mxRectangle dimensionsA = graph.getPreferredSizeForCell(sommetA);
                 mxRectangle dimensionsB = graph.getPreferredSizeForCell(sommetB);
@@ -103,8 +112,11 @@ public class DendrogrammTemplate {
                 graph.resizeCell(sommetB, dimensionsB);
                 graph.resizeCell(sommetAB, dimensionsAB);
 
-                Object arcA_AB = graph.insertEdge(parent, null,null,sommetA,sommetAB);
-                Object arcB_AB = graph.insertEdge(parent, null,null,sommetB,sommetAB);
+                mxCell cellA = (mxCell) sommetA;
+                mxCell cellB = (mxCell) sommetB;
+
+                Object arcA_AB = graph.insertEdge(feuille, null,null,sommetA,sommetAB);
+                Object arcB_AB = graph.insertEdge(feuille, null,null,sommetB,sommetAB);
 
                 values.remove(classeAB);
                 keys.remove(classeAB);
@@ -115,34 +127,59 @@ public class DendrogrammTemplate {
                 keys= pair.getKeys();
 
 
-
             }
 
 
             System.out.println(nModule +" -> "+ clusterService.getModules().size());
             clusterService.getModules().forEach(System.err::println);
 
-            showGraph(graph);
+            showGraph(graph, feuille);
         });
     }
 
 
 
-    private void showGraph(mxGraph graph)
+    private void showGraph(mxGraph graph, Object feuille)
     {
-        mxHierarchicalLayout layout = new mxHierarchicalLayout(graph);
-        layout.setIntraCellSpacing(100); // Espacement entre les nœuds dans la même couche
-        layout.setInterRankCellSpacing(150); // Espacement entre les couches
-        layout.execute(parent);
+//        mxHierarchicalLayout layout = new mxHierarchicalLayout(graph);
+//        layout.setIntraCellSpacing(100); // Espacement entre les nœuds dans la même couche
+//        layout.setInterRankCellSpacing(150); // Espacement entre les couches
+//        layout.execute(feuille);
+        organizeCellsManually(graph, feuille);
         graph.getModel().endUpdate();
         mxGraphComponent graphComponent = new mxGraphComponent(graph);
+        graphComponent.setPreferredSize(new Dimension(800, 800));frame.getContentPane().add(graphComponent);
         frame.getContentPane().add(graphComponent);
+        graphComponent.zoomTo(0.5,true);
         frame.pack();
         frame.setVisible(true);
     }
 
 
 
+    private void organizeCellsManually(mxGraph graph, Object parent) {
+        Object[] cells = graph.getChildVertices(parent);
+        int j = 0;
+        int x0 = 100;
+        int y = 100;
+
+        for (Object cell : cells) {
+            mxCell cell2 = (mxCell) cell;
+            if(! (cell2.getValue() instanceof Float)) {
+                graph.getModel().setGeometry(cell, new mxGeometry(x0, 100, 150, 30));
+                x0 += 200;
+
+            }else{
+                mxCell cell3 = (mxCell) cells[j-1];
+                mxCell cell4 = (mxCell) cells[j-2];
+                float test = (float) Math.abs((cell3.getGeometry().getX()-cell4.getGeometry().getX()));
+                float min = (float) Math.min(cell3.getGeometry().getX(),cell4.getGeometry().getX());
+                y += 100;
+                graph.getModel().setGeometry(cell, new mxGeometry((double) min+(test /2), y, 5, 5));
+            }
+            j+=1;
+        }
+    }
 
 
     private mxGraph  initGraph(JFrame frame)
@@ -154,7 +191,7 @@ public class DendrogrammTemplate {
         graph.setCellsResizable(true); // Désactiver le redimensionnement
         graph.setDropEnabled(true); // Désactiver le glisser-déposer
         graph.setSplitEnabled(true); // Désactiver la division
-        parent = graph.getDefaultParent();
+        feuille = graph.getDefaultParent();
         graph.getModel().beginUpdate();
         return graph;
     }
